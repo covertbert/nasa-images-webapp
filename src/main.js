32549a04eee872f9973a47224b05144305aeab6c
Bertie Blackman <blackmanrgh@gmail.com>
2018-02-03 13:33:35 +0000

    adds landing page styles and imports Material Design dark theme

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueMaterial from 'vue-material'
 import router from './router'
+import VueMaterial from 'vue-material'
 import VueParticles from 'vue-particles'
 import 'vue-material/dist/vue-material.min.css'
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>Home</h1>
+  <main class="main-home-section">
+
+    <vue-particles class="main-home-section__particles"
+                   color="#ffffff"
+                   :lineLinked="false"
+                   :hoverEffect="false"
+                   :moveSpeed="1.1"></vue-particles>
+
+    <md-card class="search-container">
+      <md-field>
+        <label>Search NASA images...</label>
+        <md-input v-model="inputValue" class="search-container__input"></md-input>
+      </md-field>
+      <md-button class="md-raised md-primary search-container__button">Search</md-button>
+    </md-card>
+  </main>
 </template>
 
 <script type="text/babel">
@@ -12,7 +27,9 @@
     props: {},
 
     data () {
-      return {}
+      return {
+        inputValue: ''
+      }
     },
 
     computed: {},
@@ -32,5 +49,41 @@
 </script>
 
 <style scoped lang="scss">
+  @import "../assets/scss/variables";
 
+  .main-home-section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: $black;
+
+    &__particles {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .search-container {
+    padding: 1rem 2rem;
+    width: 28rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: none;
+    background-color: rgba($theme-gray, 0.9);
+
+    &__input::after {
+    }
+
+    &__button {
+      margin-left: 3rem;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
 </style>
