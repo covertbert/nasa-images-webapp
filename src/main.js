c752b4595b5a87b153b3f322f92f7e8bc757e732
Bertie Blackman <blackmanrgh@gmail.com>
2018-02-03 11:26:45 +0000

    adds Material Design dependencies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueMaterial from 'vue-material'
 import router from './router'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
